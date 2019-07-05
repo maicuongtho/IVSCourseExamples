8c6cf3862954609d3b044460472130e00d75e000
maicuongtho <thomc@ntu.edu.vn>
2019-07-05 14:02:51 +0700

    <%@ page isELIgnored="false" %> để sửu dụng EL trong Maven

--- SpringMVCBai1Maven/src/main/java/jp/ivs/controller/WelcomeController.java
+++ SpringMVCBai1Maven/src/main/java/jp/ivs/controller/WelcomeController.java
@@ -1,15 +1,14 @@
 package jp.ivs.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WelcomeController {
-	@RequestMapping("welcome")
-	public String welcome(HttpServletRequest req) {
-		req.setAttribute("msg", "He lo");
+	@RequestMapping("/welcome")
+	public String welcome(ModelMap model) {
+		model.addAttribute("msg", "Helo maven");
 		return "viewWelcome";
 	}
 }
--- SpringMVCBai1Maven/src/main/webapp/WEB-INF/views/viewWelcome.jsp
+++ SpringMVCBai1Maven/src/main/webapp/WEB-INF/views/viewWelcome.jsp
@@ -1,4 +1,5 @@
 <%@ page pageEncoding="UTF-8"%>
+<%@ page isELIgnored="false" %>
 <!DOCTYPE html>
 <html>
 <head>
